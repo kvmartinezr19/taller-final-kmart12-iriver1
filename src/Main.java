45225e5197b3ed0bd2dbe21b067903ec032e25af
AnerolRivera <inriver1@poligran.edu.co>
2024-09-05 13:27:04 -0500

    Se suben cambios finales

--- src/Main.java
+++ src/Main.java
@@ -624,13 +624,25 @@ public class Main {
                                 System.out.print("** ¡Hemos pensado en un numero de 1 a 100!                  **\n");
                                 Random random = new Random();
                                 int secreto= random.nextInt(100)+1;
-                                int intentos =0;
-                                int contador = 0;
+                                int intento =10;
+                                boolean numero= false;
                                 System.out.print("** Intenta adivinarlo.....                                  **\n");
-
-
+                                do{
+                                System.out.print("** Introduce un número por favor                            **\n");
+                                    intento= scanner.nextInt();
+                                    if(intento < secreto){
+                                System.out.print("** Muy bajo, Intenta de nuevo.                              **\n");
+                                    } else if (intento > secreto) {
+                                System.out.print("** Muy alto, Intenta de nuevo.                              **\n");
+                                    } else {
+                                System.out.print("**¡Felicidades! adivinaste el numero es:" + secreto +        "\n");
+                                scanner.nextLine();
+                                numero= true;
+                                    }
+                                } while (!numero);
 
                                 System.out.print("**************************************************************\n");
+                                scanner.nextLine();
                                 break;
                             case 0:
                                 System.out.println("Retornando al Menú Principal");
@@ -702,8 +714,59 @@ public class Main {
                     }
                     break;
                 case 10:
-                    System.out.println("opc10 ");
+                    int opFor = -1;
+                    while (true) {
+                    System.out.print("**************************************************************\n");
+                    System.out.print("**                           FOR                            **\n");
+                    System.out.print("**************************************************************\n");
+                    System.out.print("**           1.   Condicional FOR                           **\n");
+                    System.out.print("**           2.   Programa de FOR                           **\n");
+                    System.out.print("**           0.   Retornar al Menu Principal                **\n");
+                    System.out.print("**************************************************************\n");
+                    System.out.print("Digita una opción: ");
+                    opFor = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (opFor) {
+                        case 1:
+                            System.out.print("**************************************************************\n");
+                            System.out.print("**                   CONDICIONAL FOR                        **\n");
+                            System.out.print("**************************************************************\n");
+                            System.out.print("** la sentencia for en java es una estructura de control    **\n");
+                            System.out.print("** de ciclo que se utiliza para ejecutar un bloque de       **\n");
+                            System.out.print("** codigo una cantidad determinada de veces.                **\n");
+                            System.out.print("**                                                          **\n");
+                            System.out.print("**  Ejemplo:                                                **\n");
+                            System.out.print("**   for (inicialización; condición; actualización){        **\n");
+                            System.out.print("**     //Bloque de codigo a ejecutar                        **\n");
+                            System.out.print("**    }                                                     **\n");
+                            System.out.print("**************************************************************\n");
+                            break;
+                        case 2:
+                            System.out.print("**************************************************************\n");
+                            System.out.print("**                      PROGRAMA FOR                        **\n");
+                            System.out.print("**************************************************************\n");
+                            System.out.print("**              Ejercicio sumatoria de numeros              **\n");
+                            System.out.print("** Ingresa un numero para realizar la sumatoria:            **\n");
+                            int numero= scanner.nextInt();
+                            int suma = 0;
+                            for (int i= 1; i<=numero; i++){
+                                suma += i;
+                            }
+                            System.out.print("** La suma del 1 al:" + numero + "es: " +  suma  +           "\n");
+                            System.out.print("**************************************************************\n");
+                            scanner.nextLine();
+                            break;
+                        case 0:
+                            System.out.println("Retornando al Menú Principal");
+                            break;
+                        default:
+                            System.out.println("Opción no válida. Ingresa una opción entre el 0 y 2");
+                    }
+                    System.out.print("Presiona Enter para continuar...");
+                    scanner.nextLine();
                     break;
+                }
+                break;
                 default:
                     System.out.println("Opción no válida. Ingresa una opción entre el 0 y 10");
             }
